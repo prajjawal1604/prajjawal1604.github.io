35c08e71a67fb804433d4569e39e1553eeac3c6a
Prajjawal Pandit <panditprajjawal@gmail.com>
2020-04-02 18:55:52 +0530

    2/4

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+
+$(window).scroll(function(){
+  $(".content1").css("opacity", 1 - $(window).scrollTop() / 350);
+});
+
+$(window).scroll(function(){
+  $(".content").css("opacity", 1 - $(window).scrollTop() / 800);
+});
