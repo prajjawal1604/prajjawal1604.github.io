55a4550b6cb6fc251a84ae78d8ec6785ad637a19
Prajjawal Pandit <panditprajjawal@gmail.com>
2020-04-05 01:59:45 +0530

    5/4

---
+++ java script/index.js
@@ -0,0 +1,56 @@
+/* ..................................navigation bar....................*/
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+/* .................................background fading....................*/
+
+
+$(window).scroll(function(){
+  $(".content1").css("opacity", 1 - $(window).scrollTop() / 350);
+});
+
+$(window).scroll(function(){
+  $(".content").css("opacity", 1 - $(window).scrollTop() / 800);
+});
+
+/* .................................side navigation bar....................*/
+
+
+// $(window).scroll(function(){
+//  if($(window).scrollTop()>814){
+//    $("#sidebar").css("display",none);
+//    console("hi");
+//   //$(".sidebar").css("top", 1000);
+//   // alert("stop"); 
+//  }
+// });
+
+
+$(window).scroll(function(){
+  $(".sidebar").css("opacity", $(window).scrollTop() / 900);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
